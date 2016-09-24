4889814db502f74b54339b6f889a55cb6804329a
Benjamin P Lee <benjaminplee@gmail.com>
2016-09-24 14:59:34 -0500

    Working sample app requesting rooms via java ews library

--- src/main/java/com/yardspoon/sandbox/ewsjavaspike/Application.java
+++ src/main/java/com/yardspoon/sandbox/ewsjavaspike/Application.java
@@ -1,7 +1,42 @@
 package com.yardspoon.sandbox.ewsjavaspike;
 
+import microsoft.exchange.webservices.data.core.ExchangeService;
+import microsoft.exchange.webservices.data.core.enumeration.misc.ExchangeVersion;
+import microsoft.exchange.webservices.data.core.enumeration.misc.TraceFlags;
+import microsoft.exchange.webservices.data.credential.ExchangeCredentials;
+import microsoft.exchange.webservices.data.credential.WebCredentials;
+import microsoft.exchange.webservices.data.misc.ITraceListener;
+import microsoft.exchange.webservices.data.property.complex.EmailAddress;
+
+import java.io.FileInputStream;
+import java.net.URI;
+import java.nio.channels.Pipe;
+import java.util.Collection;
+import java.util.EnumSet;
+import java.util.Properties;
+
 public class Application {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws Exception {
+        System.out.println("Running!");
+
+        Properties properties = new Properties();
+        properties.load(new FileInputStream("ews.properties"));
+
+        ExchangeService service = new ExchangeService();
+        ExchangeCredentials credentials = new WebCredentials(properties.getProperty("username"), properties.getProperty("password"));
+        service.setCredentials(credentials);
+        service.setUrl(new URI(properties.getProperty("url")));
+        service.setTraceEnabled(true);
+        service.setTraceFlags(EnumSet.allOf(TraceFlags.class));
+        service.setTraceListener((traceType, traceMessage) -> System.out.println("Type:" + traceType + " Message:" + traceMessage));
+
+        Collection<EmailAddress> rooms = service.getRooms(new EmailAddress(properties.getProperty("rooms_address")));
+
+        System.out.println("Found rooms:");
+        for (EmailAddress room : rooms) {
+            System.out.println(" - " + room.getAddress());
+        }
+
+        System.out.println("Stopped.");
     }
 }
